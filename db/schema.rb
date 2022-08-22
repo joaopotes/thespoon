81915ff53603d44f013e353f189ec003158caab4
Joao Potes <joao.cpotes@gmail.com>
2022-08-22 21:12:41 +0100

    Added address to restaurant

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_22_200905) do
+ActiveRecord::Schema.define(version: 2022_08_22_201147) do
 
   create_table "restaurants", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.string "address"
   end
 
 end
