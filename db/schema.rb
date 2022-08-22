5fbb5f627f372626eb51c0c2662a0181ed561e10
Joao Potes <joao.cpotes@gmail.com>
2022-08-22 21:44:05 +0100

    rails new + setup of restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_22_201147) do
+ActiveRecord::Schema.define(version: 2022_08_22_202637) do
 
   create_table "restaurants", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "address"
+    t.integer "rating"
   end
 
 end
